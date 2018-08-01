972caebaccbc00ae35b8e56ff92ad77978ba4f63
tkiguess <ahmedtafkir@gmail.com>
2018-08-01 11:50:56 -0400

    Initial commit

---
+++ src/main/java/com/example/springboooot_08/Springboooot08Application.java
@@ -0,0 +1,12 @@
+package com.example.springboooot_08;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboooot08Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboooot08Application.class, args);
+	}
+}
